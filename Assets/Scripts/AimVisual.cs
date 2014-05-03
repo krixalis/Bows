3dc64d6dda9c2aa9acfd36a4c7fc4916d47c8e3f
krixalis <krixdev@gmail.com>
2014-05-03 12:54:13 +0200

    Initial commit.
    Messy from testing bow aim.
    Need to fix Vector3.zero aim when camera raycast hits nothing.

---
+++ Assets/Scripts/AimVisual.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AimVisual : MonoBehaviour {
+
+	Ray cameraRay;
+	Plane cameraPlane;
+	float dist;
+	Vector3 pos;
+
+	public Camera playerCamera;
+
+
+	// Use this for initialization
+	void Start () {	
+		dist = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float Y = transform.rotation.y;
+		float Z = transform.rotation.z;
+
+
+		pos = Input.mousePosition;
+		cameraRay = playerCamera.ScreenPointToRay (pos);
+		//cameraPlane = new Plane(Camera.main.transform.forward, transform.position);
+
+		RaycastHit hit;
+		Physics.Raycast (cameraRay, out hit, 100);
+
+
+
+		Debug.DrawLine (cameraRay.origin, hit.point);
+		this.transform.LookAt (cameraRay.origin + cameraRay.direction * hit.distance);//(cameraRay.origin + cameraRay.direction * dist);
+		//transform.rotation.Set(transform.rotation.x, Y, Z, transform.rotation.w);
+	}
+}
